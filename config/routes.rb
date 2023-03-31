ea1d5c0d57bd26d7d46ad34e41d02d72cb279483
ifedan <danielonyejesi@gmail.com>
2023-03-31 10:13:27 +0100

    Added welcome and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/welcome'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "about", to: "pages#about"
 end
